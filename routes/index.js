98db503c9f9026654bea1ea57e48d31dcb284455
Heider Zapa <heiderzapa78@gmail.com>
2020-03-07 13:23:20 -0500

    /

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,8 @@ var router = require('express').Router();
 
 router.use('/api', require('./api'));
 
+router.use('/', (req, res) => {
+    res.send("b-observer app v1")
+})
+
 module.exports = router;
